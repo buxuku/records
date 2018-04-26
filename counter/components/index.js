30c341481343c95120faf81c888e66519420b648
林晓东 <buxuku@gmail.com>
2018-04-26 14:59:20 +0800

    add other two function

--- counter/components/index.js
+++ counter/components/index.js
@@ -4,13 +4,22 @@ class Counter extends React.Component{
     constructor(props) {
         super(props);
     }
+    incrementIfOdd = () => {
+        if(this.props.value % 2 === 0) return;
+        this.props.increment();
+    }
+    incrementSync = () => {
+        setTimeout(this.props.increment, 1000);
+    }
     render() {
         const {value, increment, decrement} = this.props;
         return (
             <div>
                 <p>{value}</p>
                 <button onClick={increment}>increment</button>{' '}
-                <button onClick={decrement}>decrement</button>
+                <button onClick={decrement}>decrement</button>{' '}
+                <button onClick={this.incrementIfOdd}>incrementIfOdd</button>{' '}
+                <button onClick={this.incrementSync}>incrementSync</button>{' '}
             </div>
         )
     }
