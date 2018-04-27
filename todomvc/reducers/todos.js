ff7607931a70248e630fb5a0a24b827eb8af69c2
林晓东 <buxuku@gmail.com>
2018-04-27 15:56:18 +0800

    set id for todos

--- todomvc/reducers/todos.js
+++ todomvc/reducers/todos.js
@@ -1,11 +1,17 @@
 const defaultState = [{
-    text: 'stuey redux'
+    text: 'stuey redux',
+    completed: false,
+    id: 0
 }];
 
 export default function todos(state = defaultState, action) {
     switch(action.type) {
         case 'ADD_TODO':
-        return [{text: action.text}, ...state];
+        return [{
+            text: action.text,
+            completed: false,
+            id: state.reduce((maxId, todo) => Math.max(maxId, todo.id), -1) +1,
+        }, ...state];
         default:
         return state;
     }
