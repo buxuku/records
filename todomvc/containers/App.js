3df61ad3f0fac0585dccf04ea567349df4e58f90
林晓东 <buxuku@gmail.com>
2018-04-27 11:25:55 +0800

    add containers file

---
+++ todomvc/containers/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div>ok</div>
+    )
+}
+
+export default App;
--- todomvc/reducers/index.js
+++ todomvc/reducers/index.js
@@ -2,7 +2,7 @@ import { combineReducers } from 'redux';
 import todos from './todos';
 
 const rootReducer = combineReducers({
-    todo,
+    todos,
 })
 
 export default rootReducer;
