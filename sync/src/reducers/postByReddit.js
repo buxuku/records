805f378bd55c77d85c836ca36227ffd036e53367
buxuku <buxuku@gmail.com>
2018-04-29 15:40:40 +0800

    add post function

--- sync/src/reducers/postByReddit.js
+++ sync/src/reducers/postByReddit.js
@@ -1,7 +1,28 @@
+import { RECEIVE_POSTS, REQUEST_POSTS,INVALIDATE_REDDIT } from '../actions';
+
+function posts(state = {
+    isFetching: false,
+    invalidate: false,
+    item: [],
+}, action) {
+    switch(action.type) {
+        case INVALIDATE_REDDIT:
+        return Object.assign({}, state, {didInvalidate: true});
+        case REQUEST_POSTS:
+        return Object.assign({}, state, {isFetching: true});
+        case RECEIVE_POSTS:
+        return Object.assign({}, state, {isFetching:false, didInvalidate: false, items: action.posts, lastUpdate: action.updateAt})
+    }
+}
+
 export default function postByReddit(state = {}, action) {
     switch(action.type) {
-        case 'test':
-        return state;
+        case RECEIVE_POSTS:
+        case REQUEST_POSTS:
+        case INVALIDATE_REDDIT:
+        return Object.assign({}, state, {
+            [action.reddit]: posts(state[action.reddit],action)
+        });
         default:
         return state;
     }
